cc9d8cc730fd90cca89e7ac35f9b920486f3952b
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2023-10-01 21:06:18 +0000

    fix: ajustes no coletor e no storage manager

--- src/spotify_integration/collector.py
+++ src/spotify_integration/collector.py
@@ -9,13 +9,19 @@ class Collector:
         pass
 
     def get_artists(self, artist_name, offset, limit):
-        return self.client.find_artist(artist_name=artist_name, offset=offset, limit=limit)
+        artists_response = self.client.find_artist(artist_name=artist_name, offset=offset, limit=limit)
+
+        return artists_response["artists"].get("items", [])
 
     def get_albums(self, artist_id, offset, limit):
-        return self.client.get_artist_albums(artist_id=artist_id, offset=offset, limit=limit)
+        albums_response = self.client.get_artist_albums(artist_id=artist_id, offset=offset, limit=limit)
+
+        return albums_response.get("items", [])
 
     def get_tracks(self, album_id, offset, limit):
-        return self.client.get_albums_tracks(album_id=album_id, offset=offset, limit=limit)
+        tracks_response = self.client.get_albums_tracks(album_id=album_id, offset=offset, limit=limit)
+
+        return tracks_response.get("items", [])
 
     def get_credits(self, track_id):
         publicClient = SpotifyPublicClient()
--- src/spotify_integration/storage_manager.py
+++ src/spotify_integration/storage_manager.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from typing import Optional
 
+from pprint import pprint
+
 
 class StorageManager:
     __FILE_NAME = "arquivos/coleta_dados_spotify.xlsx"
@@ -14,14 +16,24 @@ class StorageManager:
         self.lyrics_df = pd.DataFrame()
 
     def save_artists(self, artists):
+        for artist in artists:
+            del artist["images"]
+
         new_artists = pd.DataFrame.from_dict(artists)
         self.artists_df = pd.concat([self.artists_df, new_artists], ignore_index=True)
 
     def save_albums(self, albums):
+        for album in albums:
+            del album["images"]
+            del album["artists"]
+
         new_albums = pd.DataFrame.from_dict(albums)
         self.albums_df = pd.concat([self.albums_df, new_albums], ignore_index=True)
 
     def save_songs(self, songs):
+        for song in songs:
+            del song["artists"]
+
         new_songs = pd.DataFrame.from_dict(songs)
         self.songs_df = pd.concat([self.songs_df, new_songs], ignore_index=True)
 
@@ -54,5 +66,5 @@ class StorageManager:
         self.albums_df.drop_duplicates(subset=['id'], inplace=True)
         self.songs_df.drop_duplicates(subset=['id'], inplace=True)
         self.credits_df.drop_duplicates(subset=['id'], inplace=True)
-        self.lyrics_df.drop_duplicates(subset=['id'], inplace=True)
+        self.lyrics_df.drop_duplicates(subset=['trackUri'], inplace=True)
         
