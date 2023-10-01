a323a09c025976279e99e2b805a8d74846ea9feb
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2023-10-01 21:35:33 +0000

    feat: implementação do coletor

--- src/main.py
+++ src/main.py
@@ -1,24 +1,9 @@
-from spotify_integration.spotify_client import SpotifyClient
-from spotify_integration.spotify_public_client import SpotifyPublicClient
 from spotify_integration.collector import Collector
-from spotify_integration.storage_manager import StorageManager
-
 
 collector = Collector()
-storage_manager = StorageManager()
 
 def run():
-    artists = collector.get_artists(artist_name="Beyonce", offset=0, limit=1) # Pegar Beyonce
-    albums = collector.get_albums(artist_id="6vWDO969PvNqNYHIOW5v0m", offset=0, limit=50) # Pegar albums da Beyonce
-    tracks = collector.get_tracks(album_id="6FJxoadUE4JNVwWHghBwnb", offset=0, limit=50) # Pegar dados do album RENAISSANCE da Beyonce
-    tracks_credits = collector.get_credits(track_id="1w7cgGZR86yWz1pA2puVJD") # Créditos da mpusica HEATED
-
-    storage_manager.save_artists(artists)
-    storage_manager.save_albums(albums)
-    storage_manager.save_songs(tracks)
-    storage_manager.save_credits(tracks_credits)
-
-    storage_manager.persist()
+    collector.collect_data()
 
 
 if __name__ == "__main__":
--- src/spotify_integration/collector.py
+++ src/spotify_integration/collector.py
@@ -1,13 +1,48 @@
+import time
+
 from spotify_integration.spotify_client import SpotifyClient
 from spotify_integration.spotify_public_client import SpotifyPublicClient
+from spotify_integration.storage_manager import StorageManager
+
 
 class Collector:
     def __init__(self) -> None:
         self.client = SpotifyClient()
         self.public_client = SpotifyPublicClient()
 
+        self.storage_manager = StorageManager()
+
     def collect_data(self):
-        pass
+        start_time = time.time()
+        print("Coletando artistas")
+        artists = self.get_artists(artist_name="Beyonce", offset=0, limit=1) # Pegar Beyonce
+
+        for artist in artists:
+            print("Coletando albums")
+            albums = self.get_albums(artist_id=artist.get("id"), offset=0, limit=50) # Pegar albums da Beyonce
+            tracks = []
+
+            for album in albums:
+                print("Coletando tracks do album " + album.get("id"))
+
+                tracks = self.get_tracks(album_id=album.get("id"), offset=0, limit=50) # Pegar dados do album RENAISSANCE da Beyonce
+
+                for track in tracks:
+                    print("Coletando credits da track " + track.get("id"))
+                    tracks_credits = self.get_credits(track_id=track.get("id")) # Créditos da musica HEATED
+
+            self.storage_manager.save_albums(albums)
+            self.storage_manager.save_songs(tracks)
+            self.storage_manager.save_credits(tracks_credits)
+
+        self.storage_manager.save_artists(artists)
+
+        print("Tempo de coleta --- %s seconds ---" % (time.time() - start_time))
+
+        start_time = time.time()
+        self.storage_manager.persist()
+        print("Tempo para salvar no arquivo --- %s seconds ---" % (time.time() - start_time))
+
 
     def get_artists(self, artist_name, offset, limit):
         artists_response = self.client.find_artist(artist_name=artist_name, offset=offset, limit=limit)
