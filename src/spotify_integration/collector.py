367a74f2cc897898c86ce573e80b000ba6c7325d
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2023-10-24 00:13:49 +0000

    feat: adiciona tratamento de paginação

--- src/spotify_integration/collector.py
+++ src/spotify_integration/collector.py
@@ -7,6 +7,8 @@ from spotify_integration.storage_manager import StorageManager
 
 
 class Collector:
+    _LIMITE_REQUEST = 50
+
     def __init__(self) -> None:
         self.client = SpotifyClient()
         self.public_client = SpotifyPublicClient()
@@ -16,17 +18,24 @@ class Collector:
     def collect_data(self):
         start_time = time.time()
 
-        print("Coletando artistas")
+        print("Listando artistas")
         artists = self.list_all_artists_from_graph()
+        print("Tempo para listar artistas --- %s segundos ---" % (time.time() - start_time))
 
+        """
+        start_time = time.time()
+
+        print("Coletando dados dos artistas")
         for artist in artists:
             collect_data_for_single_artist(artist)
 
-        print("Tempo de coleta --- %s seconds ---" % (time.time() - start_time))
+        print("Tempo de coleta --- %s segundos ---" % (time.time() - start_time))
 
         start_time = time.time()
+        print("Salvando os dados em arquivo")
         self.storage_manager.persist()
-        print("Tempo para salvar no arquivo --- %s seconds ---" % (time.time() - start_time))
+        print("Tempo para salvar no arquivo --- %s segundos ---" % (time.time() - start_time))
+        """
 
     def list_all_artists_from_graph(self):
         alfabeto = list(string.ascii_lowercase)
@@ -34,7 +43,8 @@ class Collector:
         todos_os_artistas_do_grafo = []
 
         for letra in alfabeto:
-            todos_os_artistas_do_grafo += self.get_artists(artist_name=letra, offset=0, limit=50)
+            for i in range(0, 951, self._LIMITE_REQUEST):
+                todos_os_artistas_do_grafo += self.get_artists(artist_name=letra, offset=i, limit=self._LIMITE_REQUEST)
 
         todos_os_artistas_do_grafo = list({artista['id']: artista for artista in todos_os_artistas_do_grafo}.values())
 
@@ -44,14 +54,14 @@ class Collector:
 
     def collect_data_for_single_artist(self, artist):
         print("Coletando albums")
-        albums = self.get_albums(artist_id=artist.get("id"), offset=0, limit=50)
+        albums = self.get_albums(artist_id=artist.get("id"), offset=0, limit=self._LIMITE_REQUEST)
 
         tracks = []
         tracks_credits = []
 
         for album in albums:
             print("Coletando tracks do album " + album.get("id"))
-            tracks += self.get_tracks(album_id=album.get("id"), offset=0, limit=50)
+            tracks += self.get_tracks(album_id=album.get("id"), offset=0, limit=self._LIMITE_REQUEST)
 
         for track in tracks:
             print("Coletando credits da track " + track.get("id"))
