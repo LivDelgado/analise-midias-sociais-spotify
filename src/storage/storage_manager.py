9c5f4ea076ce05b0b57e0fa18139149d5197b089
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2023-11-27 22:24:21 -0300

    fix: caminho do arquivo

--- src/storage/storage_manager.py
+++ src/storage/storage_manager.py
@@ -8,7 +8,7 @@ from storage.constants import Constants
 class StorageManager:
     def __init__(self, num_planilha: int) -> None:
         self.__FILE_NAME = (
-            f"arquivos/main_artists/coleta_top_artists_{num_planilha}.xlsx"
+            f"arquivos/main_artists_dividida/coleta_top_artists_{num_planilha}.xlsx"
         )
         self.artists_df = pd.DataFrame()
         self.albums_df = pd.DataFrame()
